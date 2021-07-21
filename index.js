a65cf37603b3b44b27a4fe04b83a3af52f27ca6c
Jeff Sarpong <crazicoda@gmail.com>
2021-07-21 06:38:25 +0000

    Setting up

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const PORT = 5000 || process.env.PORT;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Kass");
+});
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
