b8cfde775ddf12c1404931f22535b1e73663fdcc
CraziCoda <64246414+CraziCoda@users.noreply.github.com>
2021-08-15 10:31:32 -0700

    updates on new pages

--- routes/route.js
+++ routes/route.js
@@ -3,7 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index.hbs", { title: "KASS" });
+  res.render("index.hbs", { title: "KASS || HOME", style: "../styles/style.css"});
+});
+router.get("/panel", (req, res)=>{
+  res.render("./pages/panel.hbs", {title: "Admin || Login", style: "../styles/login.css"});
 });
 
 module.exports = router;
--- views/script/index.js
+++ views/script/index.js
@@ -1,25 +1,25 @@
-var button = document.querySelector('.button');
+/* var button = document.querySelector('.button');
 var navigation = document.querySelector('.navigation');
 var blur = document.querySelector('.backdrop');
 
 button.addEventListener('click', rotate);
 
-let boolean = false;
+let isActive = false;
 
 function rotate(event) {
     if (!boolean) {
         button.classList.add('active');
         navigation.style.transform = 'translateX(-40%)';
-        navigation.style.transition = '300ms linear';
+        navigation
         blur.style.opacity = '1';
         blur.addEventListener('click', backagain);
-        boolean = true;
+        isActive = true;
     } else {
         button.classList.remove('active');
         navigation.style.transform = 'translateX(-100%)';
         navigation.style.transition = '300ms linear';
         blur.style.opacity = '0';
-        boolean = false;
+        isActive = false;
     }
 }
 
@@ -28,4 +28,36 @@ function backagain(event){
     navigation.style.transform = 'translateX(-100%)';
     boolean = false;
     blur.style.opacity = '0';
+} */
+
+
+const menu = document.querySelector('.navigation');
+const button = document.querySelector('.button');
+
+button.addEventListener('click', showMenu);
+
+let menuIsActive = false;
+function showMenu(){
+    switch(menuIsActive){
+        case false:
+            menuIsActive = true;
+            break;
+        case true:
+            menuIsActive = false;
+    }
+    call();
 }
+
+function call(){
+    if(menuIsActive){
+        menu.style.transition = '300ms linear';
+        menu.style.opacity = "1";
+        
+    }else{
+        menu.style.transition = '300ms linear';
+        menu.style.opacity = "0";
+    }
+}
+
+
+
